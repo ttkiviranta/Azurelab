55cda4f822dbd8efcc3198a8221b4d68d7a19866
ttkiviranta <ttkiviranta@gmail.com>
2019-05-04 06:29:23 +0300

    VUE

---
+++ Client-VUE3/wwwroot/precache-manifest.c8590d9567715474da437502c7a9fe34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c7b45b41d5679f97b75a",
+    "url": "/js/chunk-vendors.0aaa1742.js"
+  },
+  {
+    "revision": "a80134adb9f35a8ea70f",
+    "url": "/js/app.fb1e3f17.js"
+  },
+  {
+    "revision": "6662900280c509798878",
+    "url": "/js/about.3abe0522.js"
+  },
+  {
+    "revision": "f2a2d97fe329bb687fbfa18d950bd24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a80134adb9f35a8ea70f",
+    "url": "/css/app.48f5c24c.css"
+  }
+];
